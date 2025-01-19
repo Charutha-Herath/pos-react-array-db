0583b3d1983df58daea6c8a4c54f61cc9c38bc1e
Charutha-Herath <charuh2023@gmail.com>
2025-01-19 23:29:31 +0530

    add home

---
+++ src/pages/Home..tsx
@@ -0,0 +1,15 @@
+export function Home(){
+    return(
+        <>
+            <section id="home">
+                <div id="carouselExampleIndicators" className="relative w-full">
+
+                    <div className="relative overflow-hidden">
+                        <h1>Home Page</h1>
+                    </div>
+
+                </div>
+            </section>
+        </>
+    )
+}
