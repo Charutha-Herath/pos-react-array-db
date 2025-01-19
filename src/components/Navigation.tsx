a0254d039b094ea3cea872e070160f12d7a5aa36
Charutha-Herath <charuh2023@gmail.com>
2025-01-19 23:55:44 +0530

    update home and navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,13 +1,13 @@
 import {Link} from "react-router";
 import "../assets/Navigation.css"
-
+import logo from "../assets/react.svg"
 export function Navigation(){
     return (
         <>
             <header className="bg-gray-700/100 shadow-lg">
                 <nav className="px-4 py-3">
                     <div className="flex justify-between items-center">
-                        <Link className="custom-link !important font-bold font-serif text-2xl" to="/">Simplify</Link>
+                        <Link className="custom-link !important font-bold font-serif text-2xl" to="/"><img src={logo}/></Link>
                         <div className="flex space-x-10">
                             <Link className="custom-link" to="/">Home</Link>
                             <Link className="custom-link" to="/customer">Customer</Link>
