eac010cf16e71c5fa33cf7d77605233b0fe4c20a
Charutha-Herath <charuh2023@gmail.com>
2025-01-19 23:30:16 +0530

    update app adding home

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@
 import {createBrowserRouter, RouterProvider} from "react-router";
 import './App.css'
 import {RootLayout} from "./components/RootLayout.tsx";
+import {Home} from "./pages/Home..tsx";
 
 function App() {
     const routes = createBrowserRouter([
         {
             path:'',element:<RootLayout/>,children:[
-
+                {path: '/',element:<Home/>}
             ]
         }
     ])
