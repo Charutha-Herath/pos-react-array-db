b15243fa10f885ef1def7ca36f9add5bd7ea6f09
Charutha-Herath <charuh2023@gmail.com>
2025-01-21 02:01:00 +0530

    update Navigation UI

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,40 +1,41 @@
-import { Link } from "react-router";
 import "../assets/Navigation.css";
 import logo from "../assets/city super main.jpg";
 
 export function Navigation() {
     return (
         <>
-            <aside className="bg-gray-800 text-white w-64 min-h-screen fixed top-0 left-0 flex flex-col shadow-lg transform -translate-x-0">
-                <div className="flex items-center justify-center h-20 bg-gray-900">
-                    <img src={logo} alt="Logo" className="h-12" />
+            <aside className="bg-red-700 text-white w-64 min-h-screen fixed top-0 left-0 flex flex-col shadow-xl">
+                {/* Logo Section */}
+                <div className="flex items-center justify-center h-20 bg-red-900">
+                    <img src={logo} alt="Logo" className="h-12 rounded-full shadow-lg" />
                 </div>
+                {/* Navigation Menu */}
                 <nav className="flex-1">
                     <ul className="flex flex-col p-4 space-y-4">
                         <li>
-                            <Link className="custom-link block" to="/">
-                                Home
-                            </Link>
+                            <a className="custom-link block" href="/">
+                                <i className="bx bx-home-alt mr-3"></i> Home
+                            </a>
                         </li>
                         <li>
-                            <Link className="custom-link block" to="/customer">
-                                Customer
-                            </Link>
+                            <a className="custom-link block" href="/customer">
+                                <i className="bx bx-user mr-3"></i> Customer
+                            </a>
                         </li>
                         <li>
-                            <Link className="custom-link block" to="/items">
-                                Item
-                            </Link>
+                            <a className="custom-link block" href="/items">
+                                <i className="bx bx-box mr-3"></i> Item
+                            </a>
                         </li>
                         <li>
-                            <Link className="custom-link block" to="/orders">
-                                Orders
-                            </Link>
+                            <a className="custom-link block" href="/orders">
+                                <i className="bx bx-cart mr-3"></i> Orders
+                            </a>
                         </li>
                         <li>
-                            <Link to="/orderDetail" className="bg-blue-500 text-white p-3 rounded shadow-lg hover:bg-blue-600 transition block text-center">
-                                New Order
-                            </Link>
+                            <a className="custom-action-link block text-center" href="/orderDetail">
+                                <i className="bx bx-plus-circle mr-2"></i> New Order
+                            </a>
                         </li>
                     </ul>
                 </nav>
